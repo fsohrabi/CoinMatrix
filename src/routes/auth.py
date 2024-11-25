f504d2042b50c3cc8446a6aac82c399436d88c6e
fsohrabi <fs.sohrabi70@gmail.com>
2024-11-25 19:36:49 +0100

    add revoke token for jwt

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,7 +1,9 @@
 from flask import request, jsonify, Blueprint
 from marshmallow import ValidationError
+
+from src.config import Config
 from src.models.users import User
-from src.routes.helpers import add_token_to_database
+from src.routes.helpers import add_token_to_database, revoke_token, is_token_revoked
 from src.schemas.user import UserCreateSchema
 from flask_jwt_extended import (
     create_access_token,
@@ -10,7 +12,7 @@ from flask_jwt_extended import (
     get_jwt_identity,
     get_jwt,
 )
-from src import db, pwd_context
+from src import db, jwt
 from src.schemas.user_login_schema import UserLoginSchema
 
 auth_blueprint = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
@@ -59,3 +61,32 @@ def refresh():
     access_token = create_access_token(identity=current_user)
     add_token_to_database(access_token)
     return jsonify({"access_token": access_token}), 200
+
+
+@auth_blueprint.route("/revoke_access", methods=["DELETE"])
+@jwt_required()
+def revoke_access_token():
+    jti = get_jwt()["jti"]
+    user_identity = get_jwt_identity()
+    revoke_token(jti, user_identity)
+    return jsonify({"message": "token revoked"}), 200
+
+
+@auth_blueprint.route("/revoke_refresh", methods=["DELETE"])
+@jwt_required(refresh=True)
+def revoke_refresh_token():
+    jti = get_jwt()["jti"]
+    user_identity = get_jwt_identity()
+    revoke_token(jti, user_identity)
+    return jsonify({"message": "token revoked"}), 200
+
+
+@jwt.user_lookup_loader
+def user_loader_callback(jwt_payload):
+    identity = jwt_payload[Config.JWT_IDENTITY_CLAIM]
+    return User.query.get(identity)
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_revoked(jwt_payload):
+    return is_token_revoked(jwt_payload)
