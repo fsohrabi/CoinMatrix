ba380c1d159a427fa11928929f703e62c2793cc6
fsohrabi <fs.sohrabi70@gmail.com>
2024-11-25 20:10:01 +0100

    role model and that relations to user added

--- src/models/users.py
+++ src/models/users.py
@@ -10,6 +10,7 @@ class User(db.Model):
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     _password = db.Column('password', db.String(255), nullable=False)
+    roles = db.relationship("Role", secondary="user_roles", back_populates="users")
 
     @hybrid_property
     def password(self):
@@ -18,3 +19,26 @@ class User(db.Model):
     @password.setter
     def password(self, value):
         self._password = pwd_context.hash(value)
+
+    def has_role(self, role):
+        return bool(
+            Role.query
+            .join(Role.users)
+            .filter(User.id == self.id)
+            .filter(Role.slug == role)
+            .count() == 1
+        )
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(36), nullable=False)
+    slug = db.Column(db.String(36), nullable=False, unique=True)
+    users = db.relationship("User", secondary="user_roles", back_populates="roles")
+
+
+class UserRole(db.Model):
+    __tablename__ = "user_roles"
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"), primary_key=True)
