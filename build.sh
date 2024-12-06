cdfe87860ec25a8078dbf96b141a281764210d41
fsohrabi <fs.sohrabi70@gmail.com>
2024-12-06 18:45:12 +0100

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
-pip install --upgrade pip
+#!/usr/bin/env bash
+# Install Python dependencies
 pip install -r requirements.txt
-flask db upgrade
+
+# Apply database migrations
+alembic upgrade head
+
+# Any other commands your app needs to initialize (e.g., collect static files)
+echo "Build script completed."
