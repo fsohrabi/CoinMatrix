b0c721dac5767d2df324290ac7de5fe8bc33b0cb
fsohrabi <fs.sohrabi70@gmail.com>
2025-02-10 18:16:26 +0100

    change htpponly cookies to https setting

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -92,8 +92,8 @@ def login():
         add_token_to_database(refresh_token)
         # Set HTTP-only cookies
         response = make_response(jsonify({"message": "Login successful"}), 200)
-        response.set_cookie("access_token", access_token, httponly=True, secure=False, samesite="Lax")
-        response.set_cookie("refresh_token", refresh_token, httponly=True, secure=False, samesite="Lax")
+        response.set_cookie("access_token", access_token, httponly=True, secure=True, samesite="Lax")
+        response.set_cookie("refresh_token", refresh_token, httponly=True, secure=True, samesite="Lax")
         return response
     except ValidationError as err:
         return jsonify({"errors": err.messages}), 400
