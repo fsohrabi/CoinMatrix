50b7f857c0bad62cccd802bbb3f5826f9811a1e1
fsohrabi <fs.sohrabi70@gmail.com>
2025-03-26 15:09:04 +0100

    add dockerfiles and setting for that

--- frontend/README.md
+++
@@ -1,1 +0,0 @@
-# Coinmatrix-frontend
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Set the path to your repository root (one level up)
+  const rootPath = resolve(__dirname, '..')
+  // Load all environment variables from the repo root .env file
+  const env = loadEnv(mode, rootPath, '')
+
+  return {
+    plugins: [react()],
+    // Optionally expose env variables to your client code
+    define: {
+      'process.env': env,
+    },
+  }
 })
