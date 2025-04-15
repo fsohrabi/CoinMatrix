c4f9bc88413958f64236d17291ddde9daf351870
fsohrabi <fs.sohrabi70@gmail.com>
2025-04-15 13:00:02 +0200

    change vite config to read env from root directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  // Set the path to your repository root (one level up)
-  const rootPath = resolve(__dirname, '..')
-  // Load all environment variables from the repo root .env file
-  const env = loadEnv(mode, rootPath, '')
+  // Load env from parent folder manually
+  const env = loadEnv(mode, '../', '')
 
   return {
     plugins: [react()],
-    // Optionally expose env variables to your client code
     define: {
-      'process.env': env,
-    },
+      'import.meta.env': {
+        VITE_APP_API_URL: JSON.stringify(env.VITE_APP_API_URL),
+        VITE_APP_APP_NAME: JSON.stringify(env.VITE_APP_APP_NAME),
+        VITE_APP_DEBUG: JSON.stringify(env.VITE_APP_DEBUG),
+        VITE_TINYMCE_API_KEY: JSON.stringify(env.VITE_TINYMCE_API_KEY),
+      }
+    }
   }
 })
