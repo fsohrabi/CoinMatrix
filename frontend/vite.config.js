6ea880194f55500b2aaff78eaadac51352cc8bd0
fsohrabi <fs.sohrabi70@gmail.com>
2025-04-15 15:41:55 +0200

    change vite config to read .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,6 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '../', '')
-
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': {
-        VITE_APP_API_URL: JSON.stringify(env.VITE_APP_API_URL),
-        VITE_APP_APP_NAME: JSON.stringify(env.VITE_APP_APP_NAME),
-        VITE_APP_DEBUG: JSON.stringify(env.VITE_APP_DEBUG),
-        VITE_TINYMCE_API_KEY: JSON.stringify(env.VITE_TINYMCE_API_KEY),
-      }
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
